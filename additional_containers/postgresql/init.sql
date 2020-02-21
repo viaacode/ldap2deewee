7bbfed923739c6881b792f84d1f8d2a320dc5822
Mattias Poppe <poppe.mattias@gmail.com>
2020-02-21 10:26:55 +0100

    Revert "Add PostgreSQL docker container"
    This reverts commit 141a980916f82fd5f770a28235fbdd6cc4473b32.

--- additional_containers/postgresql/init.sql
+++
@@ -1,8 +0,0 @@
-CREATE TYPE entity_type AS ENUM ('org', 'person');
-CREATE TABLE entities(
-    id serial PRIMARY KEY,
-    ldap_uuid UUID,
-    type entity_type,
-    content JSON,
-    last_modified_timestamp timestamp
-);
