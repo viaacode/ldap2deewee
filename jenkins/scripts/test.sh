30fa7d001ae3a0a82a1cc5820478a1aaf0e3e53d
Mattias Poppe <poppe.mattias@gmail.com>
2020-03-05 19:11:38 +0100

    Add coverage measurement in Jenkins test script

--- jenkins/scripts/test.sh
+++ jenkins/scripts/test.sh
@@ -2,4 +2,5 @@ REGISTRY=$1
 BRANCH=$2
 VERSION=$3
 
-docker container run --name ldap2deewee_test "${REGISTRY}/ldap2deewee:${BRANCH}-${VERSION}" "-m" "pytest" "tests"
+docker container run --name ldap2deewee_test "${REGISTRY}/ldap2deewee:${BRANCH}-${VERSION}" \
+    "-m" "pytest" "--cov=app" "--cov=deewee_communication" "--cov=ldap_communication" "tests"
