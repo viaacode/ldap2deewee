723c883a4ad6b76b0952aad1651b398f3651fa12
Mattias Poppe <poppe.mattias@gmail.com>
2020-03-09 11:17:53 +0100

    Add integration-style tests

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,21 @@
 FROM python:3.7.6-slim-buster
 
-RUN apt-get update && apt-get install -y gcc libpq-dev && apt-get clean
+RUN apt-get update && apt-get install -y gcc libpq-dev postgresql-11 && apt-get clean
 
 WORKDIR /src/usr/app
 COPY . .
 
 ENV PYTHONPATH="$PYTHONPATH:/src/usr/app"
 
+# Install packages
 RUN pip3 install http://do-prd-mvn-01.do.viaa.be:8081/repository/pypi-internal/packages/viaa-chassis/0.0.4/viaa_chassis-0.0.4-py3-none-any.whl && pip3 install -r requirements.txt && pip3 install -r requirements-test.txt && pip3 install flake8
+
+# Postgresql initdb cannot be run as root
+RUN chown -R postgres:postgres /src \
+&& chmod 777 /src 
+
+# Switch USER to non-root to run
+USER postgres
 
 # Run the application
 ENTRYPOINT ["python3"]
---
+++ tests/integration/test_ldap_communication.py
@@ -0,0 +1,116 @@
+import pytest
+import ldap3
+from datetime import timedelta, datetime
+import time
+
+from viaa.configuration import ConfigParser
+
+from ldap_communication import LdapWrapper, LdapClient
+
+LDAP_SUFFIX = 'dc=hetarchief,dc=be'
+LDAP_PEOPLE = f'ou=people,{LDAP_SUFFIX}'
+LDAP_ORGS = f'ou=orgs,{LDAP_SUFFIX}'
+DN_ORG1 = f'o=meemoo_org1,{LDAP_ORGS}'
+DN_ORG2 = f'o=meemoo_org2,{LDAP_ORGS}'
+DN_PERSON1 = f'mail=meemoo_user1@meemoo.meemoo,{LDAP_PEOPLE}'
+DN_PERSON2 = f'mail=meemoo_user2@meemoo.meemoo,{LDAP_PEOPLE}'
+
+
+class LdapWrapperMock(LdapWrapper):
+
+    def __init__(self, params: dict, search_attributes=ldap3.ALL_ATTRIBUTES):
+        super().__init__(params, get_info=ldap3.OFFLINE_SLAPD_2_4, client_strategy=ldap3.MOCK_SYNC)
+        self.connection.strategy.add_entry(params['bind'], {'userPassword': params['password'], 'sn': 'admin_sn'})
+
+
+class LdapClientMock(LdapClient):
+
+    def __init__(self, params: dict):
+        self.ldap_wrapper = LdapWrapperMock(params, LdapClient.SEARCH_ATTRIBUTES)
+
+
+class TestLdapWrapperMock:
+
+    @pytest.fixture
+    def ldap_wrapper(self):
+        """Returns a LdapWrapperMock initiliazed by the parameters in config.yml"""
+        return LdapWrapperMock(ConfigParser().config['ldap'])
+
+    def test_search(self, ldap_wrapper):
+        search_result = ldap_wrapper.search(LDAP_ORGS)
+        assert search_result is not None
+
+    def test_search_invalid_search(self, ldap_wrapper):
+        with pytest.raises(ldap3.core.exceptions.LDAPInvalidDnError) as ldap_error:
+            ldap_wrapper.search('invalid')
+        assert ldap_error.value.args[0] is not None
+
+    def test_add_invalid(self, ldap_wrapper):
+        with pytest.raises(ldap3.core.exceptions.LDAPInvalidDnError) as ldap_error:
+            ldap_wrapper.add('dn')
+        assert ldap_error.value.args[0] is not None
+
+    def test_add_delete(self, ldap_wrapper):
+        dn = f'o=test1,{LDAP_ORGS}'
+        add_result = ldap_wrapper.add(dn, 'organization', {'o': 'test1'})
+        assert add_result
+        delete_result = ldap_wrapper.delete(f'o=test1,{LDAP_ORGS}')
+        assert delete_result
+
+
+class TestLdapClientMock:
+    """Tests if the client can communicate with an LDAP mock server"""
+
+    @classmethod
+    def setup_class(cls):
+        """ Create two orgs and two people"""
+        cls.ldap_client = LdapClientMock(ConfigParser().config['ldap'])
+        ldap_wrapper = cls.ldap_client.ldap_wrapper
+        ldap_wrapper.add(DN_ORG1, 'organization', {'o': 'meemoo_org1', 'modifyTimestamp': datetime.now()})
+        time.sleep(1)
+        ldap_wrapper.add(DN_ORG2, 'organization', {'o': 'meemoo_org2', 'modifyTimestamp': datetime.now()})
+
+        ldap_wrapper.add(DN_PERSON1, 'inetOrgPerson',
+                         {'mail': 'meemoo_user1@meemoo.meemoo', 'cn': 'meemoo1',
+                          'sn': 'meemoo1', 'modifyTimestamp': datetime.now()})
+        time.sleep(1)
+        ldap_wrapper.add(DN_PERSON2, 'inetOrgPerson',
+                         {'mail': 'meemoo_user2@meemoo.meemoo', 'cn': 'meemoo2',
+                          'sn': 'meemoo2', 'modifyTimestamp': datetime.now()})
+
+    def _modifytimestamp_value(self, ldap_entry):
+        """Returns the modifyTimestamp value of the LDAP entry"""
+        return ldap_entry.modifyTimestamp.value
+
+    def test_search_ldap_orgs(self):
+        ldap_orgs = self.ldap_client.search_ldap_orgs()
+        assert len(ldap_orgs) == 2
+        assert any(ldap_entry.entry_dn == DN_ORG2 for ldap_entry in ldap_orgs)
+
+        max_timestamp = max(ldap_orgs, key=self._modifytimestamp_value).modifyTimestamp.value
+        # Timestamp greater than last modified should result in no results
+        search_timestamp = max_timestamp + timedelta(microseconds=1)
+        assert len(self.ldap_client.search_ldap_orgs(search_timestamp)) == 0  # Asserts list empty
+
+        # Timestamp lesser than last modified should result in DN_ORG2
+        search_timestamp = max_timestamp - timedelta(microseconds=1)
+        ldap_orgs_filtered = self.ldap_client.search_ldap_orgs(search_timestamp)
+        assert len(ldap_orgs_filtered) == 1
+        assert ldap_orgs_filtered[0].entry_dn == DN_ORG2
+
+    def test_search_ldap_people(self):
+        ldap_people = self.ldap_client.search_ldap_people()
+
+        assert len(ldap_people) == 2
+        assert any(ldap_entry.entry_dn == DN_PERSON2 for ldap_entry in ldap_people)
+
+        max_timestamp = max(ldap_people, key=self._modifytimestamp_value).modifyTimestamp.value
+        # Timestamp greater than last modified should result in no results
+        search_timestamp = max_timestamp + timedelta(microseconds=1)
+        assert len(self.ldap_client.search_ldap_people(search_timestamp)) == 0  # Asserts list empty
+
+        # Timestamp lesser than last modified should result in DN_PERSON2
+        search_timestamp = max_timestamp - timedelta(microseconds=1)
+        ldap_people_filtered = self.ldap_client.search_ldap_people(search_timestamp)
+        assert len(ldap_people_filtered) == 1
+        assert ldap_people_filtered[0].entry_dn == DN_PERSON2
