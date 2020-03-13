fd196dfb90dc67daf77867f4ffd318819dae3057
Tina <schizoid69@gmail.com>
2020-03-13 13:05:31 +0100

    Update build.sh

--- jenkins/scripts/build.sh
+++ jenkins/scripts/build.sh
@@ -3,4 +3,4 @@ BRANCH=$2
 VERSION=$3
 
 cp ./config.yml.example ./config.yml
-docker build -t "${REGISTRY}/ldap2deewee:${BRANCH}-${VERSION}" .
+docker build -t "${REGISTRY}/viaa-tools/ldap2deewee:${VERSION}" .
